9befaa068c8b4e64a9732e8720dcce7d2de04e6c
monya-9 <lee99714@naver.com>
2020-06-01 13:33:37 +0900

    아이콘 버튼 SceneBuilder 로 만들기 (미완)

---
+++ src/fxml/button_pack/ButtonController.java
@@ -0,0 +1,15 @@
+package fxml.button_pack;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ButtonController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+}
